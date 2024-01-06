5c33cf274f89e2e27f29fbbde6956845b223857d
quypq0420 <phanquangquy9a5@gmail.com>
2024-01-06 18:53:30 +0700

    set up docker and prisma

---
+++ prisma/db.js
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client';
+
+const prismaClientSingleton = () => {
+    return new PrismaClient();
+};
+
+const globalForPrisma = globalThis;
+
+/**
+ * @type {PrismaClient}
+ */
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+// e.g. in `pages/index.tsx`
+/*
+import prisma from './db'
+
+export const getServerSideProps = async () => {
+  const posts = await prisma.post.findMany()
+
+  return { props: { posts } }
+}
+*/
